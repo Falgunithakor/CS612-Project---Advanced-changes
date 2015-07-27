0976480f242bde6e230ccb3fde80e6ef06bbc6a0
Falguni Thakor <thako001@cougars.csusm.edu>
2015-07-27 10:32:28 -0700

    Standard Scaler results
    StandardScaler results csv files

--- src/Main.py
+++ src/Main.py
@@ -31,26 +31,29 @@ normalizers = [None,NumpyNormalizer(), ScikitNormalizer(),MinMaxScaler()]
                        SelectKBest(f_regression,k=40)]
 '''
 
-feature_eliminators = [LinearSVC(),
-                       ExtraTreesClassifier(compute_importances=True, random_state=0)]
+feature_eliminators = [VarianceThreshold(),
+                       ExtraTreesClassifier(compute_importances=True, random_state=0),
+                       RFE(estimator=SVR(kernel="linear"),n_features_to_select=5),
+                       RFECV(estimator=SVR(kernel="linear")), TruncatedSVD(),
+                        ]
 
 #the_models = [linear_model.BayesianRidge(), svm.SVR(), svm.SVR(kernel='rbf')]
 the_models = [linear_model.BayesianRidge(), svm.SVR(), RandomForestRegressor(), LinearRegression()]
 
-normalizers = [None
+normalizers = [StandardScaler()
                 #,NumpyNormalizer()#, ScikitNormalizer()
                 #, MinMaxScaler() ,Binarizer()
                 # ,Imputer(), KernelCenterer()
-                # ,Normalizer() ,StandardScaler()
+                # ,Normalizer() ,
                 ]
 for normalizer in normalizers:
-    #for feature_eliminator in feature_eliminators:
-    for k_value in range(5, 20):
+    for feature_eliminator in feature_eliminators:
+    #for k_value in range(5, 20):
     #for k_value in range(13, 14):
         for the_model in the_models:
             file_path = "../Datasets/HIV_37_Samples/MergedDataset.csv"
             loaded_data = FileLoader.load_file(file_path)
-            feature_eliminator = SelectKBest(f_regression,k=k_value)
+            #feature_eliminator = SelectKBest(f_regression,k=k_value)
             the_data_manager = DataManager(feature_eliminator,normalizer=normalizer)
             the_data_manager.set_data(loaded_data)
             the_data_manager.split_data(test_split=0.15, train_split=0.70)
@@ -59,8 +62,8 @@ for normalizer in normalizers:
 
             exp.run_experiment()
             if(exp.get_r2(SplitTypes.Train) > 0 and exp.get_r2(SplitTypes.Valid) > 0 and exp.get_r2(SplitTypes.Test) >  0):
-                print(k_value,
-                #feature_eliminator.get_support(indices=True),
+                print(
+                feature_eliminator.get_support(indices=True),
                 type(normalizer).__name__, type(feature_eliminator).__name__ ,
                 type(the_model).__name__,
                 "Fitness", exp.fitness_matrix[0],
